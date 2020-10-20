27c4b9ac019363bed2d28c4a51ec2cfabad4ec37
zhukangkang <776362265@qq.com>
2020-10-20 10:35:51 +0800

    初始化 flutter 项目

---
+++ android/app/src/main/kotlin/com/example/kk_flutter01/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kk_flutter01
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Flutter'),
+        ),
+        body: Center(
+          child: RaisedButton(
+            onPressed: () {
+              const url = 'https://www.baidu.com';
+              launch(url);
+            },
+            child: Text('Go To Bai Du'),
+          ),
+        ),
+      ),
+    );
+  }
+}
