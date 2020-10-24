7efe903c62e0b6e6c2ee9af3f1fa36e1c9f20d6c
zhukangkang <776362265@qq.com>
2020-10-24 11:06:07 +0800

    add：新增容器组件

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// import 'dart:html';
 import 'package:flutter/material.dart';
 import 'pages/weather_page.dart';
 
@@ -9,74 +10,37 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Dio 请求',
+      title: '容器组件示例',
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Dio 请求'),
+          title: Text('容器组件示例'),
         ),
-        body: WeatherPage(),
+        body: KKTest(),
       ),
     );
   }
 }
 
-// class MyApp extends StatelessWidget {
-//   // This widget is the root of your application.
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         primarySwatch: Colors.blue,
-//         visualDensity: VisualDensity.adaptivePlatformDensity,
-//       ),
-//       home: MyHomePage(title: 'Flutter Demo Home Page'),
-//     );
-//   }
-// }
-
-// class MyHomePage extends StatefulWidget {
-//   MyHomePage({Key key, this.title}) : super(key: key);
-//   final String title;
-
-//   @override
-//   _MyHomePageState createState() => _MyHomePageState();
-// }
-
-// class _MyHomePageState extends State<MyHomePage> {
-//   int _counter = 0;
-
-//   void _incrementCounter() {
-//     setState(() {
-//       _counter++;
-//     });
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text(widget.title),
-//       ),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headline4,
-//             ),
-//           ],
-//         ),
-//       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
-//     );
-//   }
-// }
+class KKTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200,
+      height: 200,
+      // 添加装饰效果
+      decoration: BoxDecoration(
+        color: Colors.red,
+        border: new Border.all(
+          color: Colors.green,
+          width: 8.0, //边框粗细
+        ),
+        borderRadius: const BorderRadius.all(const Radius.circular(8.0)), //边框弧度
+      ),
+      child: Text(
+        'Flutter',
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 28),
+      ),
+    );
+  }
+}
