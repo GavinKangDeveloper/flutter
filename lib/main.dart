8f3336d10426bd0ff8b45487a512863be445dd09
zhukangkang <776362265@qq.com>
2020-11-05 22:11:28 +0800

    add：新增 单击和侧滑删除的手势

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,85 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    title: '滑动删除示例',
+    home: KKDismissibleTest(),
+  ));
 }
 
+// TODO:侧滑删除
+class KKDismissibleTest extends StatelessWidget {
+  List<String> items = List<String>.generate(30, (index) => "列表项 ${index + 1}");
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('滑动删除示例'),
+      ),
+      body: ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context, index) {
+          final item = items[index];
+          return Dismissible(
+              key: Key(item),
+              onDismissed: (direction) {
+                items.removeAt(index);
+                Scaffold.of(context).showSnackBar(
+                  SnackBar(content: Text("$item 被删除了")),
+                );
+              },
+              child: ListTile(
+                title: Text('$item'),
+              ));
+        },
+      ),
+    );
+  }
+}
+
+// TODO：单击手势
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '单击手势手势交互',
       theme: ThemeData(
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: KKGestureTest(),
+    );
+  }
+}
+
+class KKGestureTest extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('单击手势'),
+      ),
+      body: Center(child: KKCustomButton()),
+    );
+  }
+}
+
+class KKCustomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        final snackBar = SnackBar(content: Text('单击'));
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+      child: Container(
+        padding: EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: Theme.of(context).buttonColor,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Text('测试手势的按钮'),
+      ),
     );
   }
 }
